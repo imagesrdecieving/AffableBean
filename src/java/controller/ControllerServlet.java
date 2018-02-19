21f77a62d2ff0a3823c4d5095d287212d1646416
image <image@DESKTOP-NISORBH>
2018-02-19 03:36:45 -0500

    Netbeans Eccomerce Tutorials 10-11 complete

--- src/java/controller/ControllerServlet.java
+++ src/java/controller/ControllerServlet.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+ *
+ * You may not modify, use, reproduce, or distribute this software
+ * except in compliance with the terms of the license at:
+ * http://developer.sun.com/berkeley_license.html
+ */
+
 package controller;
 
 import cart.ShoppingCart;
@@ -5,23 +13,21 @@ import entity.Category;
 import entity.Product;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Locale;
 import java.util.Map;
 import javax.ejb.EJB;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import validate.Validator;
+import javax.servlet.http.*;
 import session.CategoryFacade;
 import session.OrderManager;
 import session.ProductFacade;
+import validate.Validator;
 
 /**
  *
- * @author Anthony Catalano amcatalano@mail.fhsu.edu
+ * @author tgiunipero
  */
 @WebServlet(name = "Controller",
             loadOnStartup = 1,
@@ -43,6 +49,7 @@ public class ControllerServlet extends HttpServlet {
     @EJB
     private OrderManager orderManager;
 
+
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
 
@@ -54,6 +61,7 @@ public class ControllerServlet extends HttpServlet {
         // store category list in servlet context
         getServletContext().setAttribute("categories", categoryFacade.findAll());
     }
+
 
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -70,6 +78,7 @@ public class ControllerServlet extends HttpServlet {
         HttpSession session = request.getSession();
         Category selectedCategory;
         Collection<Product> categoryProducts;
+
 
         // if category page is requested
         if (userPath.equals("/category")) {
@@ -120,8 +129,29 @@ public class ControllerServlet extends HttpServlet {
 
         // if user switches language
         } else if (userPath.equals("/chooseLanguage")) {
-            // TODO: Implement language request
 
+            // get language choice
+            String language = request.getParameter("language");
+
+            // place in request scope
+            request.setAttribute("language", language);
+
+            String userView = (String) session.getAttribute("view");
+
+            if ((userView != null) &&
+                (!userView.equals("/index"))) {     // index.jsp exists outside 'view' folder
+                                                    // so must be forwarded separately
+                userPath = userView;
+            } else {
+
+                // if previous view is index or cannot be determined, send user to welcome page
+                try {
+                    request.getRequestDispatcher("/index.jsp").forward(request, response);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                return;
+            }
         }
 
         // use RequestDispatcher to forward request internally
@@ -154,6 +184,7 @@ public class ControllerServlet extends HttpServlet {
         ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
         Validator validator = new Validator();
 
+
         // if addToCart action is called
         if (userPath.equals("/addToCart")) {
 
@@ -185,8 +216,8 @@ public class ControllerServlet extends HttpServlet {
             String quantity = request.getParameter("quantity");
 
             boolean invalidEntry = validator.validateQuantity(productId, quantity);
-            
-                if (!invalidEntry) {
+
+            if (!invalidEntry) {
 
                 Product product = productFacade.find(Integer.parseInt(productId));
                 cart.update(product, quantity);
@@ -225,11 +256,25 @@ public class ControllerServlet extends HttpServlet {
                     // if order processed successfully send user to confirmation page
                     if (orderId != 0) {
 
+                        // in case language was set using toggle, get language choice before destroying session
+                        Locale locale = (Locale) session.getAttribute("javax.servlet.jsp.jstl.fmt.locale.session");
+                        String language = "";
+
+                        if (locale != null) {
+
+                            language = (String) locale.getLanguage();
+                        }
+
                         // dissociate shopping cart from session
                         cart = null;
 
                         // end session
                         session.invalidate();
+
+                        if (!language.isEmpty()) {                       // if user changed language using the toggle,
+                                                                         // reset the language attribute - otherwise
+                            request.setAttribute("language", language);  // language will be switched on confirmation page!
+                        }
 
                         // get order details
                         Map orderMap = orderManager.getOrderDetails(orderId);
